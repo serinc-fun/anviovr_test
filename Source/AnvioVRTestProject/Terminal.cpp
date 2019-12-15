1e5a9ed57132b0ba9787b632a69154ea75333f66
Serinc <ser0inc@gmail.com>
2019-12-15 23:08:05 +0300

    Added terminal animation; Added dissolve to BP_Key;

--- Source/AnvioVRTestProject/Terminal.cpp
+++ Source/AnvioVRTestProject/Terminal.cpp
@@ -6,6 +6,24 @@
 #include "Components/BoxComponent.h"
 #include "Components/ArrowComponent.h"
 #include "AnvioVRTestProjectCharacter.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
+void FMaterialAnimationColorData::UpdateCurrentColor(UMaterialInstanceDynamic* InMID)
+{
+	if (InMID && InMID->IsValidLowLevel())
+	{
+		FMaterialParameterInfo ParameterInfo(ParameterName);
+		InMID->GetVectorParameterValue(ParameterInfo, CurrentColor);
+	}
+}
+
+void FMaterialAnimationColorData::BlendColorData(UMaterialInstanceDynamic* InMID, float InBlendValue)
+{
+	if (InMID && InMID->IsValidLowLevel())
+	{
+		InMID->SetVectorParameterValue(ParameterName, FMath::Lerp(CurrentColor, TargetColor, InBlendValue));
+	}
+}
 
 ATerminal::ATerminal()
 {
@@ -21,17 +39,40 @@ ATerminal::ATerminal()
 	ButtonCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("ButtonCollision"));
 	ButtonCollision->SetupAttachment(ButtonCollision);
 
-	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));	
+}
+
+void ATerminal::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	if (ButtonMID == nullptr)
+	{
+		ButtonMID = ButtonMesh->CreateAndSetMaterialInstanceDynamic(0);
+	}
+
+	AnimationPulseColor.ParameterName = ParamNameColor;
+	AnimationGridColor.ParameterName = ParamNameSubColor;
+	
+	AnimationTimeline.AddInterpFloat(CurvePush, FOnTimelineFloatStatic::CreateUObject(this, &ATerminal::OnUpdatePushCurve));
+	AnimationTimeline.AddInterpFloat(CurveColor, FOnTimelineFloatStatic::CreateUObject(this, &ATerminal::OnUpdateColorCurve));
+
+	OnInteracted(bTerminalActivated, true);
+}
+
+void ATerminal::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (AnimationTimeline.IsPlaying())
+	{
+		AnimationTimeline.TickTimeline(DeltaSeconds);
+	}
 }
 
 bool ATerminal::CanInteract_Implementation(AAnvioVRTestProjectCharacter* InInstigator)
 {
-	if (InInstigator && InInstigator->IsValidLowLevel())
-	{
-		return !InInstigator->IsInventoryEmpty();
-	}
-
-	return false;
+	return true;
 }
 
 void ATerminal::Interact_Implementation(AAnvioVRTestProjectCharacter* InInstigator)
@@ -41,18 +82,49 @@ void ATerminal::Interact_Implementation(AAnvioVRTestProjectCharacter* InInstigat
 		auto InstigatorInventory = InInstigator->GetInventoryItems();
 		for (auto& TargetItem : InstigatorInventory)
 		{
-			if (TargetItem.GetObject() && (Key.GetObject() && TargetItem == Key || TargetItem.GetObject()->GetClass() == KeyType))
+			if (TargetItem.GetObject() && (Key && TargetItem.GetObject() == Key || Key == nullptr && TargetItem.GetObject()->GetClass() == KeyType))
 			{
 				if (InInstigator->RemoveFromInventory(TargetItem))
 				{
 					bTerminalActivated = true;
-					OnInteracted(true);
-
-					return;
+					break;
 				}				
 			}
 		}
 	}
+	
+	OnInteracted(bTerminalActivated);
+}
 
-	OnInteracted(false);
+void ATerminal::OnInteracted_Implementation(bool IsSuccess, bool Force /* false */)
+{
+	AnimationPulseColor.TargetColor = IsSuccess ? ColorPulseActivated : ColorPulseDeactivated;
+	AnimationPulseColor.UpdateCurrentColor(ButtonMID);
+
+	AnimationGridColor.TargetColor = IsSuccess ? ColorGridActivated : ColorGridDeactivated;
+	AnimationGridColor.UpdateCurrentColor(ButtonMID);
+
+	if (Force)
+	{
+		OnUpdateColorCurve(1.0f);
+	}
+	else
+	{
+		AnimationTimeline.PlayFromStart();
+		ButtonMesh->PlayAnimation(PushAnimation, false);
+	}
+}
+
+void ATerminal::OnUpdatePushCurve(float Value)
+{
+	if (ButtonMID && ButtonMID->IsValidLowLevel())
+	{
+		ButtonMID->SetScalarParameterValue(ParamNamePush, Value);
+	}
+}
+
+void ATerminal::OnUpdateColorCurve(float Value)
+{
+	AnimationPulseColor.BlendColorData(ButtonMID, Value);
+	AnimationGridColor.BlendColorData(ButtonMID, Value);
 }
--- Source/AnvioVRTestProject/Terminal.h
+++ Source/AnvioVRTestProject/Terminal.h
@@ -5,11 +5,33 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Interactable.h"
+#include "Components/TimelineComponent.h"
 #include "Terminal.generated.h"
 
+class UAnimationAsset;
 class USkeletalMeshComponent;
 class UBoxComponent;
 class UArrowComponent;
+
+USTRUCT()
+struct FMaterialAnimationColorData
+{
+	GENERATED_USTRUCT_BODY();
+
+	UPROPERTY(EditAnywhere)
+	FName ParameterName;
+
+	UPROPERTY(EditAnywhere)
+	FLinearColor TargetColor;
+
+	void UpdateCurrentColor(UMaterialInstanceDynamic* InMID);
+	void BlendColorData(UMaterialInstanceDynamic* InMID, float InBlendValue);
+	
+private:
+	
+	UPROPERTY(EditAnywhere)
+	FLinearColor CurrentColor;
+};
 
 UCLASS()
 class ANVIOVRTESTPROJECT_API ATerminal
@@ -24,6 +46,9 @@ public:
 
 protected:
 
+	virtual void PostInitializeComponents() override;
+	virtual void Tick(float DeltaSeconds) override;
+	
 	virtual bool CanInteract_Implementation(AAnvioVRTestProjectCharacter* InInstigator) override;
 	virtual void Interact_Implementation(AAnvioVRTestProjectCharacter* InInstigator) override;
 	
@@ -46,11 +71,59 @@ protected:
 	TSubclassOf<AActor> KeyType;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Access)
-	TScriptInterface<IInteractable> Key;
+	AActor* Key;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Access)
 	bool bTerminalActivated = false;
 
-	UFUNCTION(BlueprintImplementableEvent, Category = Activation)
-	void OnInteracted(bool IsSuccess);
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	UCurveFloat* CurvePush;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FName ParamNamePush;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	UCurveFloat* CurveColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FName ParamNameColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FName ParamNameSubColor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FLinearColor ColorPulseActivated;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FLinearColor ColorPulseDeactivated;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FLinearColor ColorGridActivated;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	FLinearColor ColorGridDeactivated;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Animation)
+	UAnimationAsset* PushAnimation;
+	
+	UFUNCTION(BlueprintNativeEvent, Category = Animation)
+	void OnInteracted(bool IsSuccess, bool Force = false);
+
+	UFUNCTION()
+	void OnUpdatePushCurve(float Value);
+
+	UFUNCTION()
+	void OnUpdateColorCurve(float Value);
+	
+	UPROPERTY(Transient)
+	FTimeline AnimationTimeline;
+
+	UPROPERTY(Transient)
+	FMaterialAnimationColorData AnimationPulseColor;
+
+	UPROPERTY(Transient)
+	FMaterialAnimationColorData AnimationGridColor;
+	
+	UPROPERTY(Transient)
+	UMaterialInstanceDynamic* ButtonMID;
 };
